784585d880667564060dadb4634aad69e89c346b
JohannesNichola <johannesnichola975@gmail.com>
2025-09-09 22:46:18 +0700

    new commit

--- main/urls.py
+++ main/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index'),          # /
-    path('products/', views.product_list),        # /products/
-    path('products/<int:id>/', views.product_detail), # /products/1/
+    path('', views.index, name='index'),          
+    path('products/', views.product_list),        
+    path('products/<int:id>/', views.product_detail), 
 ]
